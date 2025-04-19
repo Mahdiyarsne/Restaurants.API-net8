dc86f3b514c905c06b328974283db6ad5e620877
Mahdiyar <mahdiyarsne98@gmail.com>
2025-04-19 16:24:18 +0330

    Async

---
+++ Restaurants.Infrastucture/Extensions/ServiceCollectionExtenson.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Restaurants.Infrastucture.Persistence;
+
+namespace Restaurants.Infrastucture.Extensions;
+
+public static class ServiceCollectionExtenson 
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<RestautantDbContext>(options =>
+          options.UseSqlServer(configuration.GetConnectionString("RestaurantDb"))
+        ); 
+    }
+}
---
+++ Restaurants.Infrastucture/Persistence/RestautantDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastucture.Persistence;
+
+public class RestautantDbContext(DbContextOptions<RestautantDbContext> options) : DbContext(options)
+{
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Dish> Dishes { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Restaurant>()
+              .OwnsOne(r => r.Address);
+
+        modelBuilder.Entity<Restaurant>()
+            .HasMany(r => r.Dishes)
+            .WithOne()
+            .HasForeignKey(d => d.RestaurantId);
+
+    }
+}
